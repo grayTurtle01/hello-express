f8b286ec60533d1fb206757d0ecf5c7e7da32e11
grayTurtle01 <the.correo@protonmail.com>
2022-10-08 05:36:08 -0500

    hello render from Express

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ let express = require('express')
 app = express()
 
 app.get("/", (req,res) => {
-    //~ res.json( {msg: "Hello Express"} )
-    res.end( "<h1>Hello Express</h1>" )
+    res.json( {msg: "Hello Render from Express"} )
+    //~ res.end( "<h1>Hello Express</h1>" )
 })
 
 let PORT = process.env.PORT || 8000
